9e445098e7bdee17e89733b5281df3fc0f099dcf
kirankv8641 <kv878432@gmail.com>
2026-02-26 12:53:36 +0530

    yet to fix

--- db.js
+++ db.js
@@ -1,17 +1,12 @@
- const mongoose=require('mongoose');
+const mongoose = require("mongoose"); 
 
-//  define mongoDB connection url
- const mongoURL='mongodb://localhost:27017/hotels'
+ 
+ const mongoURL='mongodb+srv://kv878432_db_user:kirankv@cluster0.ocowqjk.mongodb.net/'
 
- //set up mongoDB connection
-mongoose.connect("mongodb://127.0.0.1:27017/hotels")
+ mongoose.connect(mongoURL)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-
- //Get the default connection
- //Mongoose maintains a default connection 
- // object representing the MonGoDB connection.
  const db=mongoose.connection;
 
  //Define event listener for database connection
